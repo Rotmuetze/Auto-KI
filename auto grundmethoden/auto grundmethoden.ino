1dc4598f963a86902b7928bcbdb53bdf4ebc130e
Benjamin Wende <benni.wende@gmail.com>
2022-11-29 12:57:12 +0100

    Update auto grundmethoden.ino

--- auto grundmethoden/auto grundmethoden.ino
+++ auto grundmethoden/auto grundmethoden.ino
@@ -7,11 +7,11 @@
 HUSKYLENS huskylens;
 
 
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64  
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-#define OLED_RESET -1      
-#define SCREEN_ADDRESS 0x3C 
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Gleichstrommotor links
@@ -49,11 +49,11 @@ void setup() {
   huskylens.writeAlgorithm(ALGORITHM_LINE_TRACKING);
   delay(100);
 
-//
+  //
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
-      ; 
+      ;
   }
 
 
@@ -79,20 +79,20 @@ void loop() {
 
       while (huskylens.available() > 0) {  //Solange die Huskylens mehr als 0 Objekte vor sich erkennt...
         HUSKYLENSResult result = huskylens.read();
-        if (result.xTarget <= 179 && result.xTarget >= 141) {         //geradeauss
+        if (result.xTarget <= 179 && result.xTarget >= 141) {  //geradeauss
           command = "fahren";
           Serial.println("fahren");
           fahren();
         }
 
-        else if (result.xTarget <= 110 && result.xTarget >= 0) {      //links
+        else if (result.xTarget <= 110 && result.xTarget >= 0) {  //links
           Serial.println("links");
           command = "links";
           links();
 
         }
 
-        else if (result.xTarget >= 210) {                            //rechts
+        else if (result.xTarget >= 210) {  //rechts
           Serial.println("rechts");
           command = "rechts";
           rechts();
@@ -110,19 +110,18 @@ void loop() {
   }
 }
 
-void fahren(){        
-               //fahren motorsteuerrung
- digitalWrite(in1, 0);
-          digitalWrite(in2, 1);
-          digitalWrite(in3, 0);
-          digitalWrite(in4, 1);
+void fahren() {
+  //fahren motorsteuerrung
+  digitalWrite(in1, 0);
+  digitalWrite(in2, 1);
+  digitalWrite(in3, 0);
+  digitalWrite(in4, 1);
 
-          analogWrite(GSM1, 175);
-          analogWrite(GSM2, 175);
-          delay(1);
-          
+  analogWrite(GSM1, 175);
+  analogWrite(GSM2, 175);
+  delay(1);
 }
-void rechts() {                      //rechts motorsteuerung
+void rechts() {  //rechts motorsteuerung
   digitalWrite(in1, 0);
   digitalWrite(in2, 1);
 
@@ -136,7 +135,7 @@ void rechts() {                      //rechts motorsteuerung
   delay(1);
 }
 
-void links() {                    //links motorsteuerung
+void links() {  //links motorsteuerung
   digitalWrite(in1, 0);
   digitalWrite(in2, 0);
 
